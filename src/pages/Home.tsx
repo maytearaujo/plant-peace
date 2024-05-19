89be7de0e8814327adc583dcbf2882b856c71f06
Rebeca <rebecaciriaca@gmail.com>
2024-05-18 22:05:38 -0300

    fix: ajustando carrossel duplicado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,11 +23,6 @@ const Home = () => {
           <h1 className='font-ebGaramond font-bold text-64 text-primaryLunarGreen capitalize'>Plants<span className='text-primaryAvacado'> in sale</span></h1>
           {/* <InSale/> */}
         </div>
-      <div>
-        <h1 className='font-ebGaramond font-bold text-64 text-primaryLunarGreen capitalize'>This weeks most popular<span className='text-primaryAvacado'>and best selling</span></h1>
-        <MostPopular />
-        <h1 className='font-ebGaramond font-bold text-64 text-primaryLunarGreen capitalize'>Plants<span className='text-primaryAvacado'>in sale</span></h1>
-      </div>
 
     </div>
 

7633d6c336049141a04159101e22acaa8b7e06a7
Maytê <maytearaujo@yahoo.com.br>
2024-05-19 13:02:39 -0300

    perf: Removendo comentarios desnecessarios

--- src/components/PlantsCard.tsx
+++ src/components/PlantsCard.tsx
@@ -1,12 +1,3 @@
-// import axios from 'axios'
-// import { useState, useEffect, Fragment } from 'react'
-
-// import photo1 from '../../src/assets/home/3this/photo 1.png';
-// import photo2 from '../../src/assets/home/3this/Photo 2.png';
-// import photo3 from '../../src/assets/home/3this/Photo 3.png';
-// import photo4 from '../../src/assets/home/3this/Photo 4.png';
-
-
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -18,38 +9,6 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 import imagensJSON from '../../src/assets/data/plants.json'; // Importando o JSON
-
-// const PlantsCard = (props: string) => {
-//     const [list, setList] = useState([]);
-
-//     useEffect(() => {
-//         axios.get('http://localhost:3000/plants')
-//             .then((response) => {
-//                 setList(response.data.plants)
-//                 console.log(list)
-//             })
-//     }, []);
-
-//     const images = [photo1, photo2, photo3, photo4]
-//     return (
-//         <Fragment>
-//             {list.length > 0 ? (
-//                 <Fragment>
-//                     <img src={images[props.num]} alt="Plants" />
-//                     <p>{list[props.num.name]}</p>
-//                     <p>{list[props.num.price]}</p>
-//                     <p>{list[props.num.label]}</p>
-
-//                 </Fragment>
-
-//             ) : (
-//                 <p> Carregando Plantas</p>
-//             )
-//             }
-//         </Fragment>
-
-//     )
-// }
 
 const PlantsCard = () => {
 

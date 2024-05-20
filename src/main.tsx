03c06b688105f577dc97558a8cff000724e48f51
ale00gs <agonsalvessissa@gmail.com>
2024-05-20 00:57:27 -0300

    Fix: atualiza a página about us e arruma o readme

--- src/main.tsx
+++ src/main.tsx
@@ -12,10 +12,10 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
       </ClerkProvider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 )
